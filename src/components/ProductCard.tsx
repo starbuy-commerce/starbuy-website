1560e9fa25253238246a7b94ebdbb904662e284f
Victor Betoni <victorbetoni@protonmail.com>
2022-01-17 03:00:04 -0300

    Adicionando mais alguns itens para teste enquanto nao implemento essa função na api :p

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,10 +10,10 @@ const ProductCard = ({ img, name, price}: Props) => {
 
     return (
         <>
-            <div className="flex-grow flex-shrink cursor-pointer justify-center box-border flex-row w-40 max-w-[10rem] md:max-w-[13rem] md:w-52 md:shadow-md border-yellow-400 rounded-lg hover:border shadow-gray-300">
+            <div className="flex-grow cursor-pointer justify-center box-border flex-row w-40 max-w-[10rem] md:max-w-[13rem] md:w-52 md:shadow-md border-yellow-400 rounded-lg border shadow-gray-300 transition ease-in-out hover:border-violet-700 delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
                 <img src={img} className="p-4 w-40 h-40 md:h-52 md:w-52 rounded-t-md" />
                 <div className="w-40 h-28 md:w-52 md:h-24">
-                    <p className="text-sm break-words font-inter font-normal mt-2 ml-2 mr-2">{formated}</p>
+                    <p className="text-sm break-words font-inter font-light mt-2 ml-2 mr-2 text-gray-800">{formated}</p>
                     <p className="font-inter font-bold text-violet-900 static mt-4 ml-2">R$ {price}</p>
                 </div>
             </div>
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -5,6 +5,9 @@ import tech from "../../images/category/tech.svg"
 import clothes from "../../images/category/clothes.svg"
 import books from "../../images/category/books.svg"
 import bass from "../../images/test/bass.jpg"
+import ampli from "../../images/test/ampli.jpg"
+import guitar from "../../images/test/guitar.jpg"
+import notebook from "../../images/test/notebook.png"
 
 const Home = () => (
     <>
@@ -18,10 +21,9 @@ const Home = () => (
 
             <div className="flex gap-6 flex-wrap md:pr-24 md:pl-24 md:gap-y-7 mt-12 justify-center">
                 <ProductCard img={bass} name="Contrabaixo Fender 032 8900 Squier Modified Jaguar Bass 538" price={2899.00} />
-                <ProductCard img={bass} name="Contrabaixo Fender 032 8900 Squier Modified Jaguar Bass 538" price={2899.00} />
-                <ProductCard img={bass} name="Contrabaixo Fender 032 8900 Squier Modified Jaguar Bass 538" price={2899.00} />
-                <ProductCard img={bass} name="Contrabaixo Fender 032 8900 Squier Modified Jaguar Bass 538" price={2899.00} />
-                <ProductCard img={bass} name="Contrabaixo Fender 032 8900 Squier Modified Jaguar Bass 538" price={2899.00} />
+                <ProductCard img={ampli} name="AMPLIFICADOR COMBO FENDER MUSTANG I V2 8 POL 20W RMS C/ EFEITO" price={1890.00} />
+                <ProductCard img={guitar} name="Fender Classic Series '72 Telecaster Thinline Electric Guitar Natural" price={1230.00} />
+                <ProductCard img={notebook} name="ASUS ROG Zephyrus S17 (2020) Laptop para jogos, 17,3' 300Hz IPS Type FHD, NVIDIA GeForce RTX 2080S, Intel Core i7-10875H, 32GB DDR4, 1TB PCIe SSD, Per-Key RGB KB, Thunderbolt 3, Windows 10, GX701LXS-XS78" price={59340.00} />
             </div>
         </div>
     </>
