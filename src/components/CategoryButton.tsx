a446434789b7f0a4817066c31ba5a948331b357c
Victor Betoni <victorbetoni@protonmail.com>
2022-03-02 18:14:17 -0300

    Corrigindo botão de redirecionamento não funfando

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -10,14 +10,12 @@ type Props = {
 const CategoryButton = ({img, category, size, id}: Props) => {
 
     return (
-        <Link to={"../category/" + id} onClick={() => window.location.reload()}>
-            <div className="cursor-pointer flew-grow">
-                <div className="rounded-full border-4 border-violet-600 w-14 h-14 flex mx-auto hover:bg-yellow-300 transition duration-300 ease-in-out">
-                    <img className={`mx-auto justify-center my-auto ${size}`} src={img}/>
-                </div>
-                <p className="text-violet-700 text-sm font-semibold text-center">{category}</p>
+        <div className="cursor-pointer flew-grow" onClick={() => window.location.href="/category/" + id}>
+            <div className="rounded-full border-4 border-violet-600 w-14 h-14 flex mx-auto hover:bg-yellow-300 transition duration-300 ease-in-out">
+                <img className={`mx-auto justify-center my-auto ${size}`} src={img}/>
             </div>
-        </Link>
+            <p className="text-violet-700 text-sm font-semibold text-center">{category}</p>
+        </div>
     );
 }
 
