a98e6f72648c316b1a905fe527eea9b68e2b11ee
Victor Betoni <victorbetoni@protonmail.com>
2022-03-14 15:07:41 -0300

    Corrigindo pagina de usuario

--- src/components/views/User.tsx
+++ src/components/views/User.tsx
@@ -6,8 +6,10 @@ import Navbar from "../Navbar";
 const proxy = 'https://blooming-coast-08475.herokuapp.com/'
 
 const User = () => {
-
-    const [user, setUser] = useState<any>({})
+    
+    const [name, setName] = useState<string>("")
+    const [city, setCity] = useState<string>("")
+    const [profilePicture, setPfp] = useState<string>("")
 
     const {username} = useParams();
     const path = username == undefined ? UserStorage.getUsername() : "/" + username;
@@ -20,24 +22,28 @@ const User = () => {
                 'Access-Control-Allow-Origin': '*'
             },
         }).then(response => response.json())
-        .then(json => setUser(json))
+        .then(json => {
+            setPfp(json.profile_picture);
+            setName(json.name);
+            setCity(json.city);
+        })
         .catch(err => console.log(err))
     }, [])
 
     return (
         <div>
             <Navbar fixed={false} bottomBar={false}/>
-            <div className="w-full h-24 from-indigo-600 via-indigo-600 to-indigo-800 bg-gradient-to-r relative visible">
+            <div className="w-full h-24 from-[#6366F1] via-[#6366F1] to-[#7ED8FF] bg-gradient-to-r relative visible">
                 <div>
                     <p className="font-inter text-white absolute mt-8 ml-48 font-bold text-xl">
-                        {UserStorage.getName().toUpperCase()}
+                        {name.toUpperCase()}
                     </p>
                     <p className="font-inter text-white absolute mt-16 ml-48 font-light text-sm">
-                        {UserStorage.getCity().toUpperCase()}
+                        {city.toUpperCase()}
                     </p>
                 </div>
             </div>
-            <img src={user.profile_picture} className="rounded-full border-yellow-500 border-4 w-32 h-32 absolute top-24 ml-10"/>
+            <img src={profilePicture} className="rounded-full border-yellow-500 border-4 w-32 h-32 absolute top-24 ml-10"/>
         </div>
     )
 } 
