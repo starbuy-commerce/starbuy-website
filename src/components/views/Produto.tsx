77eb0283ef9256af0f112b1a3f23e11ac6d6375b
Victor Betoni <victorbetoni@protonmail.com>
2022-03-28 15:07:59 -0300

    Deixando os botoes do produto bonitinhos

--- src/components/views/Produto.tsx
+++ src/components/views/Produto.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../Navbar";
+import addToCart from '../../images/add-to-cart.svg';
+import buyNow from '../../images/buy-now.svg';
 
 type Props = {
     img: string,
@@ -43,31 +45,35 @@ const Produto = () => {
         
         <div>
             <Navbar fixed={true} bottomBar={true}/>
-            <div className="mt-40 bg-gray-100 p-5">
+            <div className="mt-40 bg-yellow-100 p-5">
                 <div className="flex p-5 bg-white rounded-lg">
                     <div className="w-2/6 rounded-lg border-yellow-400 ml-12 mr-12">
-                        <img src={imagem} className="p-4 h-full"/>
+                        <img src={imagem} className="p-4"/>
                     </div>
                     <div className="w-3/6">
                     <p className="text-2xl font-inter font-semibold ml-2 mr-2 text-gray-800">{title}</p>
-                        <p className="font-inter text-5xl font-normal text-gray-800 static mt-8 ml-2">R$ {preco.toFixed(2)}</p>
+                        <p className="font-inter text-4xl font-normal text-gray-800 static mt-8 ml-2">R$ {preco.toFixed(2)}</p>
                         <div className="flex">
                             <div className="w-1/2">
                                 <p className="text-sm font-inter font-semibold mt-2 ml-2 mr-2 text-gray-800">Estimativa de entrega: Seila</p>
                             </div>
-                            <div className="w-1/2 text-left">
-                                <button className="text-sm font-inter font-semibold bg-transparent hover:bg-yellow-500 text-yellow-500  hover:text-white py-2 px-3 border border-yellow-500 hover:border-transparent rounded">Adicionar ao carrinho</button>
-                            </div>
                         </div>
 
                         <p className="text-md font-inter text-justify mt-8 ml-2 mr-2 text-gray-800">{description}</p>
-                    </div>
-
-                    <div className="w-1/6">
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <p className="text-md font-inter font-bold mt-2 ml-2 mr-2 text-gray-800"></p>
+                        <div className="float-right flex mt-6">
+                            <div className="mr-4 text-sm font-inter font-semibold bg-transparent text-indigo-500 py-2 px-3 border border-indigo-500 rounded hover:cursor-pointer">
+                                <div className="flex">
+                                    <img src={addToCart} alt="" />
+                                    <span className="ml-2">Adicionar ao carrinho</span>
+                                </div>
+                            </div>
+                            <div className="text-sm font-inter font-semibold bg-transparent text-indigo-500 py-2 px-3 border border-indigo-500 rounded hover:cursor-pointer">
+                                <div className="flex">
+                                    <img src={buyNow} alt="" />
+                                    <span className="ml-2">Comprar agora</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
