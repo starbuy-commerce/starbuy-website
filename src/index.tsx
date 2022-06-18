fbe0f595757de91d21c79b6fd95208454930cfa4
Victor Betoni <victorbetoni@protonmail.com>
2022-06-18 13:37:47 -0300

    Hm

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { checkBackupHost } from './API';
+
+checkBackupHost();
 
 ReactDOM.render(
   <React.StrictMode>
